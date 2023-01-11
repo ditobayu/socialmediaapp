5e9bd307a1c902c3b6a5d517ee61388f0fd90259
Dito Bayu Adhitya <ditobayuzs1@gmail.com>
2023-01-11 23:08:07 +0700

    server done

---
+++ client/src/components/FlexBetween.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Box } from "@mui/material";
+import { styled } from "@mui/system";
+
+const FLexBetween = styled(Box)({
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+});
+
+export default FLexBetween;
---
+++ client/src/scenes/navbar/index.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import {
+  Box,
+  IconButton,
+  InputBase,
+  Typography,
+  Select,
+  MenuItem,
+  FormControl,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+import {
+  Search,
+  Message,
+  DatkMode,
+  LightMode,
+  Notifications,
+  Help,
+  Menu,
+  Close,
+} from "@mui/icons-material";
+import { useDispatch, useSelector } from "react-redux";
+import { setMode, setLogout } from "state";
+import { useNavigate } from "react-router-dom";
+import FLexBetween from "../../components/FlexBetween";
+
+const Navbar = () => {
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
